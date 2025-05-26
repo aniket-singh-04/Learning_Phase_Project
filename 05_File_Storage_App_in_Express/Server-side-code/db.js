b26a50165d6d81f3be7265fb6bdeb5013fc3f6e5
Aniket Singh <anonymousboterror0101@gmail.com>
2025-05-26 16:49:22 +0530

    adding new Ui using chatgpt and install Mongodb only

---
+++ 05_File_Storage_App_in_Express/Server-side-code/db.js
@@ -0,0 +1,16 @@
+import { MongoClient } from 'mongodb'
+
+
+const client = new MongoClient("mongodb://127.0.0.1:27017")
+
+export async function connectDB(params) {
+    await client.connect();
+    const db = client.db();
+    console.log("Database Connected");
+    return db;
+}
+process.on("SIGINT", async() =>{
+    await client.close();
+    console.log("Client Disconnected");
+    process.exit(0);
+})
